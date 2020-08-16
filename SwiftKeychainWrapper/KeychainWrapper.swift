4de4c283467360e446e79cd93a4ee6dd5fe36bec
Hakan Aydemir <hay19@tu-clausthal.de>
2020-08-16 22:48:29 +0300

    sync flag added

--- SwiftKeychainWrapper/KeychainWrapper.swift
+++ SwiftKeychainWrapper/KeychainWrapper.swift
@@ -162,16 +162,16 @@ open class KeychainWrapper {
         return numberValue.floatValue
     }
     
-    open func double(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Double? {
-        guard let numberValue = object(forKey: key, withAccessibility: accessibility) as? NSNumber else {
+    open func double(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync: Bool? = false) -> Double? {
+        guard let numberValue = object(forKey: key, withAccessibility: accessibility, cloud_sync: cloud_sync) as? NSNumber else {
             return nil
         }
         
         return numberValue.doubleValue
     }
     
-    open func bool(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool? {
-        guard let numberValue = object(forKey: key, withAccessibility: accessibility) as? NSNumber else {
+    open func bool(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync: Bool? = false) -> Bool? {
+        guard let numberValue = object(forKey: key, withAccessibility: accessibility, cloud_sync: cloud_sync) as? NSNumber else {
             return nil
         }
         
@@ -196,8 +196,8 @@ open class KeychainWrapper {
     /// - parameter forKey: The key to lookup data for.
     /// - parameter withAccessibility: Optional accessibility to use when retrieving the keychain item.
     /// - returns: The decoded object associated with the key if it exists. If no data exists, or the data found cannot be decoded, returns nil.
-    open func object(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> NSCoding? {
-        guard let keychainData = data(forKey: key, withAccessibility: accessibility) else {
+    open func object(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync: Bool? = false) -> NSCoding? {
+        guard let keychainData = data(forKey: key, withAccessibility: accessibility, cloud_sync: cloud_sync) else {
             return nil
         }
         
@@ -210,7 +210,7 @@ open class KeychainWrapper {
     /// - parameter forKey: The key to lookup data for.
     /// - parameter withAccessibility: Optional accessibility to use when retrieving the keychain item.
     /// - returns: The Data object associated with the key if it exists. If no data exists, returns nil.
-    open func data(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Data? {
+    open func data(forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Data? {
         var keychainQueryDictionary = setupKeychainQueryDictionary(forKey: key, withAccessibility: accessibility)
         
         // Limit search results to one
@@ -250,20 +250,20 @@ open class KeychainWrapper {
     
     // MARK: Public Setters
     
-    @discardableResult open func set(_ value: Int, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
-        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility)
+    @discardableResult open func set(_ value: Int, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Bool {
+        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility, cloud_sync: cloud_sync)
     }
     
-    @discardableResult open func set(_ value: Float, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
-        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility)
+    @discardableResult open func set(_ value: Float, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Bool {
+        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility,cloud_sync: cloud_sync)
     }
     
-    @discardableResult open func set(_ value: Double, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
-        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility)
+    @discardableResult open func set(_ value: Double, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Bool {
+        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility,cloud_sync: cloud_sync)
     }
     
-    @discardableResult open func set(_ value: Bool, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
-        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility)
+    @discardableResult open func set(_ value: Bool, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Bool {
+        return set(NSNumber(value: value), forKey: key, withAccessibility: accessibility,cloud_sync: cloud_sync)
     }
 
     /// Save a String value to the keychain associated with a specified key. If a String value already exists for the given key, the string will be overwritten with the new value.
@@ -272,7 +272,7 @@ open class KeychainWrapper {
     /// - parameter forKey: The key to save the String under.
     /// - parameter withAccessibility: Optional accessibility to use when setting the keychain item.
     /// - returns: True if the save was successful, false otherwise.
-    @discardableResult open func set(_ value: String, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
+    @discardableResult open func set(_ value: String, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Bool {
         if let data = value.data(using: .utf8) {
             return set(data, forKey: key, withAccessibility: accessibility)
         } else {
@@ -286,7 +286,7 @@ open class KeychainWrapper {
     /// - parameter forKey: The key to save the object under.
     /// - parameter withAccessibility: Optional accessibility to use when setting the keychain item.
     /// - returns: True if the save was successful, false otherwise.
-    @discardableResult open func set(_ value: NSCoding, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
+    @discardableResult open func set(_ value: NSCoding, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil,cloud_sync: Bool? = false) -> Bool {
         let data = NSKeyedArchiver.archivedData(withRootObject: value)
         
         return set(data, forKey: key, withAccessibility: accessibility)
@@ -298,7 +298,7 @@ open class KeychainWrapper {
     /// - parameter forKey: The key to save the object under.
     /// - parameter withAccessibility: Optional accessibility to use when setting the keychain item.
     /// - returns: True if the save was successful, false otherwise.
-    @discardableResult open func set(_ value: Data, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil) -> Bool {
+    @discardableResult open func set(_ value: Data, forKey key: String, withAccessibility accessibility: KeychainItemAccessibility? = nil, cloud_sync : Bool? = false) -> Bool {
         var keychainQueryDictionary: [String:Any] = setupKeychainQueryDictionary(forKey: key, withAccessibility: accessibility)
         
         keychainQueryDictionary[SecValueData] = value
